849c85a7cf69cdfe5d50f02872fceaad27cd0f6d
Bas de Bruin <bruin998@gmail.com>
2019-09-03 17:15:44 +0200

    Created repeator function

--- python_basics/playsound.py
+++ python_basics/playsound.py
@@ -11,11 +11,14 @@ repeat = input("How many times would you like to hear the triangle? ")
 print("\n                 enjoy! \n ")
 
 
-#load wave file
+# load wave file
 waveObj = sa.WaveObject.from_wave_file("soundFiles/triangle1.wav")
 
+# play for each repeat function
+def repeator(repeats):
+    for i in range(int(repeats)):
+        playObj = waveObj.play()
+        playObj.wait_done()
 
-#play for each repeat
-for i in range(int(repeat)):
-    playObj = waveObj.play()
-    playObj.wait_done()
+# do it
+repeator(repeat);
