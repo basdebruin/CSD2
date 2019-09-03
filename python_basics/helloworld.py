e26ae00364e002414cf32a74f90a16d7dc0d6d8b
Bas de Bruin <bruin998@gmail.com>
2019-09-03 16:44:09 +0200

    edit to helloworld.py, with user input

--- python_basics/helloworld.py
+++ python_basics/helloworld.py
@@ -3,17 +3,19 @@
 
 
 
+#  hij vraagt de gebruiker wat z'n naam is
+name = input("Hallo, wat is je naam?  ")
 
 
 
+#  hij herhaald de naam van de gebruiker en vraagt of hij de gebruiker Sjoerd mag noemen
+antwoord = input("Ok {}, is het goed als ik je Sjoerd noem?  ".format(name))
 
 
 
-
-
-
-
-
-
-
-print("Hello World")
+#  als ze ja antwoorden is alles goed,
+#  als ze iets anders invullen dan krijgen ze ee egatief antwoord
+if antwoord == "ja":
+    print("Ok, Sjoerd")
+else:
+    print("Sorry, het goede antwoord was ja.")
