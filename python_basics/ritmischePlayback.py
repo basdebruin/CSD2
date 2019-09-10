c11028a28c64cf4e5396acb0924849b85973276a
Bas de Bruin <bruin998@gmail.com>
2019-09-10 12:28:21 +0200

    added text file input

--- python_basics/ritmischePlayback.py
+++ python_basics/ritmischePlayback.py
@@ -10,13 +10,22 @@ sound = sa.WaveObject.from_wave_file("soundFiles/triangle1.wav")
 #  Intro  /  ask for bpm
 print("\n       ---   INSTRUCTIONS   ---   \n", "\n enter your rythm values in the input.txt text file!")
 print("\n                 ---")
-bpm = input("\n enter the BPM please: ")
+bpm = float(input("\n enter the BPM please: "))
 
+# calc time.sleep time
 waitTime = 60 / bpm
 
+# read input.txt and split by space
 file = open("input.txt", "r")
 seq = file.read().split()
 
+# play
 for i in seq:
-    sound.play()
-    time.sleep(bpm / i)
+    s = sound.play()
+    # mini visualiser
+    print("#" * int(i), " " * abs(8 - int(i)) * 2, "#" * int(i))
+    # wait
+    time.sleep(waitTime / float(i))
+
+# wait till sample is done playing before terminating
+s.wait_done()
