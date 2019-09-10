13c8630e44508313400ce6973aa2713d585bcc07
Bas de Bruin <bruin998@gmail.com>
2019-09-10 13:23:29 +0200

    added a cool little visualiser

--- python_basics/ritmischePlaybackOpdracht.py
+++ python_basics/ritmischePlaybackOpdracht.py
@@ -3,7 +3,6 @@
 
 import simpleaudio as sa
 import time
-import math
 
 # load wave file
 sound = sa.WaveObject.from_wave_file("soundFiles/triangle1.wav")
@@ -16,24 +15,6 @@ bpm = float(input("\n enter the BPM please: "))
 # calc time.sleep time
 waitTime = 60 / bpm
 
-
-# small terminnal visualiser
-def visualise(t):
-    str = "";
-    for i in range(0, 60):
-        # math magic
-        wave = math.sin(i * 0.05) * math.tan(t * 0.1) * math.cos(i * t)
-        if wave > 0.2:
-            str += "#"
-        elif wave > -0.2:
-            str += " "
-        else:
-            str += "-"
-    
-    print(str, "\n", str)
-
-
-
 # read input.txt and split by space
 file = open("input.txt", "r")
 seq = file.read().split()
@@ -41,8 +22,8 @@ seq = file.read().split()
 # play
 for i in seq:
     s = sound.play()
-    #visualise
-    visualise(float(i))
+    # mini visualiser
+    print("#" * int(i), " " * abs(8 - int(i)) * 2, "#" * int(i))
     # wait
     time.sleep(waitTime / float(i))
 
