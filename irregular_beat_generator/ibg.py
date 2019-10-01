63b1c4abc69d60db817f3f55607921098a06437d
Bas de Bruin <bruin998@gmail.com>
2019-10-01 12:32:17 +0200

    Added MIDI Output

--- irregular_beat_generator/ibg.py
+++ irregular_beat_generator/ibg.py
@@ -2,9 +2,11 @@
 # bas de bruin
 
 import time
+from datetime import datetime
 import random as r
 import simpleaudio as sa
 import sys
+from midiutil import MIDIFile
 
 # load sounds
 kick = sa.WaveObject.from_wave_file("sounds/kick.wav")
@@ -43,24 +45,34 @@ playTimes = input("--- How many times would you like to hear the beat? --- (defa
 # test user input, otherwise go to default
 if bpm == "":
     bpm = 120
-else:
+elif 10 < float(bpm) < 1000:
     bpm = float(bpm)
+else:
+    bpm = 120
+
 
 timePerLoop = 60 / bpm / 4
 
 if playTimes == "":
     playTimes = 3
-elif 0 < int(playTimes) < 100:
+elif 1 < int(playTimes) < 100:
     playTimes = int(playTimes)
 else:
     playTimes = 3
 
-
+# setup midi file
+myMidi = MIDIFile(1)
+myMidi.addTempo(0,0,bpm)
+# MIDI Note Maker
+def addMidiNote(p, t):
+    # track, channnel, pitch, time(quarter note), duration, volume
+    myMidi.addNote(0, 0, p, t / 4, 0.25, 100)
 
 
 # PLAYER
 lastTime = time.time()
 index = 0
+indexFromStart = 0
 while True:
     if time.time() - lastTime >= timePerLoop and index < len(kickProbs):
 
@@ -69,17 +81,21 @@ while True:
         currentKick = float(kickProbs[index])
         if r.random() < currentKick:
             kp = kick.play()
+            addMidiNote(36, indexFromStart)
         else:
             # play hihat if kick doens't play
             hp = hat.play()
+            addMidiNote(42, indexFromStart)
         
         # snare
         currentSnare = float(snareProbs[index])
         if r.random() < currentSnare:
             sp = snare.play()
+            addMidiNote(38, indexFromStart)
         # reset lastTime for next loop
         lastTime = time.time()
         index += 1
+        indexFromStart += 1
 
     elif index >= len(kickProbs):
 
@@ -98,15 +114,15 @@ while True:
 
         time.sleep(0.001)
 
-#timestamps16th = [0, 1, 4, 5, 7, 11 etc]
-#ts = timestamps16th.pop(0)
-#while True:
-#   now = time.time() * 1000
-#   if now-timeZero >= ts * sixteenthnotedurationn
-#   print(ts)
-#   play_obj = snare.play()
-#   if timestamps16th:
-#       ts = timestams16.pop(0)
-#   else:
-#       break
-#   else: time.sleep(0.001)
+
+# WRITE MIDI
+
+print("That was all.\n")
+saveOrNot = input("--- Would you like to save the beat as a MIDI file? --- (y/n) ---> ")
+if saveOrNot.lower() == "y":
+    fileName = "Generated_Beat_" + datetime.now().strftime("%H:%M:%S_%d-%m-%y") + ".mid"
+    with open(fileName, "wb") as output_file:
+        myMidi.writeFile(output_file)
+    print("Saved as ", fileName, "\nBye!")
+else:
+    print("Okay, I won't \nBye!")
