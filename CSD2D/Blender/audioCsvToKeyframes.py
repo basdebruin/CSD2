487c501a7523089691a1ba2fad7c2fa2c8448735
basdebruin <bruin998@gmail.com>
2020-05-22 20:26:44 +0200

    py script now writes to shader material mapping data

--- CSD2D/Blender/audioCsvToKeyframes.py
+++ CSD2D/Blender/audioCsvToKeyframes.py
@@ -1,5 +1,4 @@
 import bpy
-
 import wave
 import csv
 import math
@@ -15,7 +14,7 @@ video_framerate = 24
 # open csv file
 f = open(bpy.path.abspath('//audio.csv'), 'r')
 reader = csv.reader(f)
-# make array of wave data
+# make array of wave data (mono)
 audio_frames = [i for i in reader]
 audio_frames = [float(i) for i in audio_frames[0]]
 
@@ -23,31 +22,34 @@ audio_frames = [float(i) for i in audio_frames[0]]
 
 # audio functions
 
+# get audio loudness/power at certain video frame
+# takes average of many audio frames to calc power
 def audio_at_frame(framenr, audio_frames, audio_framerate, video_framerate):
     index = int(framenr * audio_framerate / video_framerate)
     power = 0
     # take average absolute value for amplitude
-    for i in range(1000):
+    for i in range(100):
         if 0 < index+i < len(audio_frames):
             power += abs(audio_frames[index+i])
-    return power/500 
+    return power/50 
 
 
 
 
-# emission property
+# change properties of material
 
-emission = bpy.data.materials["Material"].node_tree.nodes["Principled BSDF"].inputs[17]
+# getting the z rotation property from deep within blender
+property = bpy.data.materials["blobs"].node_tree.nodes["Mapping"].inputs[2]
+property.default_value[2] = 0
 
-def setEmission(e):
-    emission.default_value = (e, e, e, 1)
-    
-last_value = 0
+# for 400 frames (the length of the audio)
+# make keyframes based on audio data
 for i in range(400):
-    val = audio_at_frame(i, audio_frames, audio_framerate, video_framerate)
-    setEmission((val + last_value)/2)
-    last_value = val
-    emission.keyframe_insert("default_value", frame=i)
+    vel = audio_at_frame(i, audio_frames, audio_framerate, video_framerate) / 1.0
+    # keep increasing the value with vel as velocity
+    property.default_value[2] += vel * vel
+    # insert keyframe at frame i
+    property.keyframe_insert("default_value", frame=i)
     
     
     
